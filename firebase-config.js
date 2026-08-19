5d2ba36ae9c948cff8f0206c6d38ceaba785f456
benobiwanless <64091959+benobiwanless@users.noreply.github.com>
2026-08-19 16:57:38 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,21 @@
-// Replace these values with the configuration
-// from your Firebase Web App.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDU0k1gR6i8mdCyPG5yl-Wf9JGyMb5M_Qw",
+  authDomain: "chatsite-5848a.firebaseapp.com",
+  projectId: "chatsite-5848a",
+  storageBucket: "chatsite-5848a.firebasestorage.app",
+  messagingSenderId: "205946782959",
+  appId: "1:205946782959:web:12f349a43ec6eabaa3471a",
+  measurementId: "G-20C85K5EFV"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
